3f4a3443cba34cb1105a99f219b1f68d52f3e3fb
Matthias Geier <mayutamano@gmail.com>
2014-07-29 10:10:10 +0200

    Add test_helper to include in test files

---
+++ lib/cargobull/test_helper.rb
@@ -0,0 +1,15 @@
+
+module Cargobull
+  module TestHelper
+    def response
+      return @response || ""
+    end
+
+    [:get, :post, :put, :patch, :delete].each do |m|
+      define_method(m) do |action, *args|
+        @response = nil
+        @response = Cargobull::Dispatch.call(m, action, *args)
+      end
+    end
+  end
+end
